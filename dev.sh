01e96cccea95e52243babbafaa820042765ae351
Andrey Rublev <version.ru@gmail.com>
2018-10-20 13:16:30 +0200

    gulp -> tup

--- dev.sh
+++ dev.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-env HUGO_ENV="DEV" gulp
-env HUGO_ENV="DEV" gulp watch &
+tup
 env HUGO_ENV="DEV" hugo server --watch --buildDrafts=true --buildFuture=true -t robust $@
--- preview.sh
+++ preview.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-env HUGO_ENV="DEV" gulp
-env HUGO_ENV="DEV" gulp watch &
+tup
 env HUGO_ENV="DEV" hugo server --watch --buildDrafts=false --buildFuture=false -t robust $@
